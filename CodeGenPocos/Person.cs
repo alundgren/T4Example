dcf67132b69a454eb41b2a37fbeedbf77383e7e8
Andreas Lundgren <andreas.lundgren@gmail.com>
2011-08-23 22:24:06 +0200

    initial

---
+++ CodeGenPocos/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeGenPocos
+{
+    public partial class Person
+    {
+        //Any code besides the properties
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+            p.FirstName = "1";
+            p.LastName = "2";
+            p.Age = 3;
+            Console.WriteLine("{0} {1} {2}", p.FirstName, p.LastName, p.Age);
+            Console.ReadKey();
+        }
+    }
+}
